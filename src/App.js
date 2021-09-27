a53c1011b84341304a88850564f266825d8e1fe2
Karin Hsin <karinhsin@gmail.com>
2021-09-27 15:49:04 +0800

    修改計數器-react-button

--- src/App.js
+++ src/App.js
@@ -2,17 +2,28 @@ import {useState} from 'react'
 
 function App() {
   //寫react前要先想好這個應用程式的狀態是什麼？（會隨著使用者改變的）
-  //[得到狀態(函式), 設定狀態(常數)] =useState(初始值)  [getter ,setter]
+  //[得到狀態(函式), 設定狀態(常數)（感變他的狀態）] =useState(初始值)  [getter ,setter]
   const [total, setTotal] = useState(0) 
 
   return (
-    <h1 onClick={() => { 
-    setTotal(total+1)
-  }}
-  >
-  {total}
-  </h1>
-  ) //起手式 中間()=>{}一定要是函式
+    <>
+      <h1>{total}</h1>
+      <button
+        onClick={() => {
+          setTotal(total + 1)
+        }}
+      >
+        +
+      </button>
+      <button
+        onClick={() => {
+          setTotal(total - 1)
+        }}
+      >
+        -
+      </button>
+    </>
+  )
 }
 
 export default App;
