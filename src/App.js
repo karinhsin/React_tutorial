a5f387e7e2d686a24ac457a66283091f57587c0d
Karin Hsin <karinhsin@gmail.com>
2021-09-27 15:25:09 +0800

    修改計數器

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg'
-import './App.css'
+import {useState} from 'react'
 
 function App() {
+  //寫react前要先想好這個應用程式的狀態是什麼？（會隨著使用者改變的）
+  //[得到狀態(函式), 設定狀態(常數)] =useState(初始值)  [getter ,setter]
+  const [total, setTotal] = useState(0) 
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <h1 onClick={() => { 
+    setTotal(total+1)
+  }}
+  >
+  {total}
+  </h1>
+  ) //起手式 中間()=>{}一定要是函式
 }
 
 export default App;
