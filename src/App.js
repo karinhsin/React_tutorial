5ae25b3a1f9bf48e3bee477d56adae81bab01591
Karin Hsin <karinhsin@gmail.com>
2021-09-28 11:22:53 +0800

    img的src的使用範例

--- src/App.js
+++ src/App.js
@@ -1,29 +1,19 @@
-import {useState} from 'react'
+import React from 'react'
+import Logo from './logo.svg'
+import { debug, devUrl, prodUrl } from './config'
 
-function App() {  //App是網頁應用程式
-  //寫react前要先想好這個應用程式的狀態是什麼？（會隨著使用者改變的）
-  //[得到狀態(函式), 設定狀態(常數)（感變他的狀態）] =useState(初始值)  [getter ,setter]
-  const [total, setTotal] = useState(0) 
+const imgUrl = debug ? devUrl : prodUrl
 
+function App() {
   return (
     <>
-      <h1>{total}</h1>
-      <button
-        onClick={() => {
-          setTotal(total + 1)
-        }}
-      >
-        +
-      </button>
-      <button
-        onClick={() => {
-          setTotal(total - 1)
-        }}
-      >
-        -
-      </button>
+      {/* 放在public資料夾，要用http://XXXX/ */}
+      <img src={`${imgUrl}/images/sky.jpeg`} alt="" />
+      {/* --------------------- */}
+      {/* 放在src資料夾，要用import(第2行) */}
+      <img src={Logo} alt="" />
     </>
   )
 }
 
-export default App;
+export default App
---
+++ src/config.js
@@ -0,0 +1,3 @@
+export const debug = true
+export const devUrl = 'http://localhost:3000'
+export const prodUrl = 'http://www.abc.com'
