052d635140919c8ea561438f34a27fb447587354
Karin Hsin <karinhsin@gmail.com>
2021-09-29 11:51:22 +0800

    解決setState異步執行-樣式一

--- src/components/CounterFC.js
+++ src/components/CounterFC.js
@@ -8,7 +8,10 @@ function CounterFC() {
         <>
             <h1
                 onClick={() => {
-                    setTotal(total + 1)
+                    const newTotal = total + 1 //同步
+                    setTotal(newTotal) // 異步，用途一：react改變網頁上的dom元素
+                    // 得到設定完成後的狀態
+                    console.log(newTotal) // 同步，用途二：主控台輸出最後的狀態值
                 }}
             >
                 {total}
