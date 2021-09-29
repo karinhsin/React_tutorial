647efcd4bc01412bcdbf2bda9b5674f2bda87127
Karin Hsin <karinhsin@gmail.com>
2021-09-29 10:21:28 +0800

    新增計數器-類別型元件範例

--- src/App.js
+++ src/App.js
@@ -1,20 +1,14 @@
-import React from 'react'
+import CounterFC from './components/CounterFC'
+import CounterCC from './components/CounterCC'
 
 function App() {
-  const count = 0
-  const foo = true
-
   return (
     <>
-      {/* https://reactjs.org/docs/conditional-rendering.html */}
-      {/* if... */}
-      {/* &&前面的要轉為布林值(一般情況下)(才不會跑出一個0) */}
-      {/* !!是強制轉成布林值 */}
-      {/* 注意：只能在jsx語法裡寫 */}
-      {count > 0 && `目前總數為${count}`}
+      <h2>函式型元件</h2>
+      <CounterFC />
       <hr />
-      {/* if...else */}
-      {foo ? 1 : 2}
+      <h2>類別型元件</h2>
+      <CounterCC />
     </>
   )
 }
---
+++ src/components/CounterCC.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+class CounterCC extends React.Component {
+    constructor() {
+        super()
+        // 定義狀態的初始值
+        this.state = {
+            total: 0,
+        }
+    }
+
+    render() {
+        return (
+            <>
+                <h1
+                    onClick={() => {
+                        this.setState({
+                            total: this.state.total + 1,
+                        })
+                    }}
+                >
+                    {this.state.total}
+                </h1>
+            </>
+        )
+    }
+}
+
+export default CounterCC
