51a5efb77067077d930063fc92346a67642980af
Karin Hsin <karinhsin@gmail.com>
2021-09-30 10:19:53 +0800

    訂購單-商品元件

--- src/components/OrderList.js
+++ src/components/OrderList.js
@@ -1,4 +1,30 @@
 import React from 'react'
+import ProductItem from './ProductItem'
+
+// 產品訂購的項目
+const products = [
+    {
+        id: 1,
+        name: '咖啡色 T-shirt',
+        category: 'Shirt',
+        image: 'https://i.imgur.com/1GrakTl.jpg',
+        price: 300,
+    },
+    {
+        id: 2,
+        name: '白色 T-shirt',
+        category: 'Shirt',
+        image: 'https://i.imgur.com/ba3tvGm.jpg',
+        price: 200,
+    },
+    {
+        id: 3,
+        name: '黑色 T-shirt',
+        category: 'Shirt',
+        image: 'https://i.imgur.com/pHQ3xT3.jpg',
+        price: 450,
+    },
+]
 
 function OrderList(props) {
     return (
@@ -16,83 +42,18 @@ function OrderList(props) {
             </div>
                     </div>
                 </div>
-                <div className="row border-top border-bottom">
-                    <div className="row main align-items-center">
-                        <div className="col-2">
-                            <img
-                                alt=""
-                                className="img-fluid"
-                                src="https://i.imgur.com/1GrakTl.jpg"
-                            />
-                        </div>
-                        <div className="col">
-                            <div className="row text-muted">Shirt</div>
-                            <div className="row">咖啡色 T-shirt</div>
-                        </div>
-                        <div className="col">
-                            <a href="#/">-</a>
-                            <a href="#/" className="border">
-                                1
-              </a>
-                            <a href="#/">+</a>
-                        </div>
-                        <div className="col">
-                            $300 <span className="close">&#10005;</span>
-                        </div>
-                    </div>
-                </div>
-                <div className="row">
-                    <div className="row main align-items-center">
-                        <div className="col-2">
-                            <img
-                                alt=""
-                                className="img-fluid"
-                                src="https://i.imgur.com/ba3tvGm.jpg"
-                            />
-                        </div>
-                        <div className="col">
-                            <div className="row text-muted">Shirt</div>
-                            <div className="row">白色 T-shirt</div>
-                        </div>
-                        <div className="col">
-                            <a href="#/">-</a>
-                            <a href="#/" className="border">
-                                1
-              </a>
-                            <a href="#/">+</a>
-                        </div>
-                        <div className="col">
-                            $200 <span className="close">&#10005;</span>
-                        </div>
-                    </div>
-                </div>
-                <div className="row border-top border-bottom">
-                    <div className="row main align-items-center">
-                        <div className="col-2">
-                            <img
-                                alt=""
-                                className="img-fluid"
-                                src="https://i.imgur.com/pHQ3xT3.jpg"
-                            />
-                        </div>
-                        <div className="col">
-                            <div className="row text-muted">Shirt</div>
-                            <div className="row">黑色 T-shirt</div>
-                        </div>
-                        <div className="col">
-                            <a href="#sec" onClick={() => { }}>
-                                -
-              </a>
-                            <a href="#sec" className="border">
-                                1
-              </a>
-                            <a href="#sec">+</a>
-                        </div>
-                        <div className="col">
-                            $450 <span className="close">&#10005;</span>
-                        </div>
-                    </div>
-                </div>
+                {products.map((v, i) => {
+                    return (
+                        <ProductItem
+                            key={v.id}
+                            name={v.name}
+                            category={v.category}
+                            image={v.image}
+                            price={v.price}
+                        />
+                    )
+                })}
+
                 <div className="back-to-shop">
                     <a href="#/">←</a>
                     <span className="text-muted">回到商品頁</span>
