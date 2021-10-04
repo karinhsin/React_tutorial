0ab8e92242fd47c6cfe2ea69fcdab1aba0bc9547
Karin Hsin <karinhsin@gmail.com>
2021-10-04 16:13:06 +0800

    生命周期-函式型元件-useEffect-didUpdate

--- src/components/LifecycleCC.js
+++ src/components/LifecycleCC.js
@@ -1,38 +1,38 @@
 import React, { Component } from 'react'
 
 class LifecycleCC extends Component {
-    constructor(props) {
-        super(props)
-        this.state = { total: 0 }
-        console.log('constructor')
-    }
+  constructor(props) {
+    super(props)
+    this.state = { total: 0 }
+    console.log('constructor')
+  }
 
-    componentDidMount() {
-        console.log('componentDidMount')
-    }
+  componentDidMount() {
+    console.log('componentDidMount')
+  }
 
-    componentDidUpdate() {
-        console.log('componentDidUpdate')
-    }
+  componentDidUpdate() {
+    console.log('componentDidUpdate')
+  }
 
-    componentWillUnmount() {
-        console.log('componentWillUnmount')
-    }
+  componentWillUnmount() {
+    console.log('componentWillUnmount')
+  }
 
-    render() {
-        console.log('render')
-        return (
-            <>
-                <h1
-                    onClick={() => {
-                        this.setState({ total: this.state.total + 1 })
-                    }}
-                >
-                    {this.state.total}
-                </h1>
-            </>
-        )
-    }
+  render() {
+    console.log('render')
+    return (
+      <>
+        <h1
+          onClick={() => {
+            this.setState({ total: this.state.total + 1 })
+          }}
+        >
+          {this.state.total}
+        </h1>
+      </>
+    )
+  }
 }
 
 export default LifecycleCC
