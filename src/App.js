e9fd09332839b23352f3eab57d3f3f39ba5ce8f1
Karin Hsin <karinhsin@gmail.com>
2021-10-06 11:00:49 +0800

    jquery整合-useEffect_useRef

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import React from 'react'
-import ProductFC from './components/ProductFC'
+import React, { useState, useEffect, useRef } from 'react'
+import $ from 'jquery' //導入jquery模組
 
+// 參考這裡的步驟說明：
+// https://github.com/eyesofkids/mfee19-react/issues/9
 function App() {
+  const [data, setData] = useState(123)
+  const buttonRef = useRef()
+
+  // didMount
+  useEffect(() => {
+    //jquery的程式碼需要寫在這裡，確保dom元素已經出現在網頁上
+    $(buttonRef.current).click(() => alert('data is ' + data))
+  }, [])
+
   return (
     <>
-      <ProductFC />
+      <button ref={buttonRef}>click me</button>
     </>
   )
 }
